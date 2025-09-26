ec8681c3849a046688d658c601aa007c7a728327
Марина Горина <mofoik@mail.ru>
2025-09-26 19:20:32 +0300

    Revert Webpack

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,12 @@
-import Game, { GameSavingData, readGameSaving as loadGame, writeGameSaving as saveGame } from './game.js';
+console.log('app worked')
 
-const game = new Game();
-game.start();
+const characters = [
+  {name: 'мечник', health: 10},
+  {name: 'маг', health: 100},
+  {name: 'маг', health: 0},
+  {name: 'лучник', health: 0},
+];
+
+const alive = characters.filter(item => item.health > 0);
+
+console.log(alive)
